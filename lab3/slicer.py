75b3299647a31d80826ac0fbaac4784a5c3f5fde
Emilee Urbanek <emileeurbanek@gmail.com>
2016-04-24 14:01:03 -0500

    added main file

---
+++ lab3/slicer.py
@@ -0,0 +1,18 @@
+from triangle import *
+"""
+Emilee Urbanek and Nick Confrey
+CMSC 22010: Digital Fabrication
+Lab 3
+Due: 4/25
+
+---
+
+The Main File -- slicer.py
+This file holds all of the main logic to make the slicer run.
+"""
+
+def main():
+    print "Nothing is done yet"
+
+if __name__ == "__main__":
+    main()
