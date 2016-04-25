7eff3193d1f44e0dd0e52cf01b92301d3be5fbbf
eurbs <emileeurbanek@gmail.com>
2016-04-24 19:23:12 -0500

    Point3Ds are hashable and constructor supports both 3 points and lists of 3 points

--- lab3/point3d.py
+++ lab3/point3d.py
@@ -7,15 +7,32 @@ Y = 1
 Z = 2
 
 class Point3D(object):
-  def __init__(self, x, y, z):
-    self.x = float(x)
-    self.y = float(y)
-    self.z = float(z)
+  """Note: Don't know how to make immutable classes, but Point3D's should be
+           treated as though they are immutable
+  """
+  def __init__(self, *args):
+    """Either can be constructed with a list of 3 numbers or 3 numbers"""
+    if len(args) == 3:
+      points = [args[0], args[1], args[2]]
+    elif isinstance(args[0], list) and len(args[0]) == 3:
+      points = args[0]
+    else:
+      raise Exception("Unacceptable arguments passed to Point3D constructor")
+    
+    self.x = float(args[0])
+    self.y = float(args[1])
+    self.z = float(args[2])
+    self.a = [self.x, self.y, self.z]
 
   def __str__(self):
     return "point: {:.5f} {:.5f} {:.5f}".format(self.x, self.y, self.z)
 
-    self.a = [self.x, self.y, self.z]
+  def __hash__(self):
+    return hash(str(self))
+
+  def __eq__(self, other):
+    """Assumes other is a Point3D as well"""
+    return (self.x == other.x) and (self.y == other.y) and (self.z == other.z)
 
   def getArray(self):
     """Returns the array representation of the point with x,y,z respectively"""
@@ -33,6 +50,11 @@ def test():
   print type(point)
   print "--------------printing a Point3D--------------"
   print point
+  print "--------------testing hashability--------------"
+  point_set = set()
+  point_set.add(point)
+  point_set.add(Point3D(1, 2, 3))
+  print "hashability is good: {}".format(len(point_set) == 1)
 
 if __name__ == "__main__":
   test()
